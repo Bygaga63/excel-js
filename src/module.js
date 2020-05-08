87d1df2c46602584cdd01289bffba1d5db428ebf
dmitry.mukhin <dmitry.mukhin@citronium.com>
2020-05-08 10:54:33 +0300

    add loaders and modules

--- src/module.js
+++ src/module.js
@@ -1,1 +1,7 @@
 console.log('bar');
+
+async function start() {
+    await Promise.resolve()
+}
+
+start().then(() => console.log('done'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,57 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');       //Для создания html файла в dist папке, добавляет тэг скрипт с бандлом
+const CopyPlugin = require('copy-webpack-plugin');              //Для копирования favicon
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); //удаляет старые файлы с папки dist
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'), // - настройка контекста, типо workdir в docker
     mode: "development",
     entry: './index.js',
     output: {
-        filename: "bundle.js",
+        filename: "bundle.[hash].js",        // - добавить хэширования js можно так
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {                             // - добавление относительных путей через alias
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        }),
+        new CopyPlugin([{
+            from: path.resolve(__dirname, 'src/favicon.ico'),
+            to: path.resolve(__dirname, 'dist'),
+        }]),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
 };
